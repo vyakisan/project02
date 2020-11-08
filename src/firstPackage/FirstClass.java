56cdd3cc2ba68f17ac4e1bea8993e64abe64efb7
vyakisan <73487331+vyakisan@users.noreply.github.com>
2020-11-08 11:12:22 -0600

    Fourth commit from remote master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -9,6 +9,7 @@ public class FirstClass {
 		System.out.println("first commit from remote master branch");
 		System.out.println("Second commit from remote master branch");
 		System.out.println("Third commit from remote master branch");
+		System.out.println("Here is the conflict we have from remote master branch");
 
 	}
 
