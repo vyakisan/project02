3a831c9e646e7c792b38ec785be1b992516e64e3
vyakisan <73487331+vyakisan@users.noreply.github.com>
2020-11-08 10:22:00 -0600

    first commit from remote master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 		System.out.println("First commit from local master branch");
 		System.out.println("second commit from local master");
 		System.out.println("third commit from local master");
+		System.out.println("first commit from remote master branch");
 
 	}
 
