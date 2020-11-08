9789773fb6b45456e3063fd18f04a4d41dc55277
Vesim <vyakisantx@gmail.com>
2020-11-08 10:09:20 -0600

    second commit from the local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("First commit from local master branch");
+		System.out.println("second commit from local master");
 
 	}
 
