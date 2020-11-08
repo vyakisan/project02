f8fae51de4296bb64c5bf99dd487c68304bbf4e7
vyakisan <73487331+vyakisan@users.noreply.github.com>
2020-11-08 11:31:37 -0600

    fifth commit from remote master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -13,6 +13,7 @@ public class FirstClass {
 		System.out.println("Here is a merge conflict I create from local master");
 
 		System.out.println("Here is the conflict we have from remote master branch");
+		System.out.println("Here is another conflict from remote master branch");
 
 	}
 
