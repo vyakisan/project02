c065591459ee0a54df753f773bf42b05b0f140c9
Vesim <vyakisantx@gmail.com>
2020-11-08 10:14:09 -0600

    third commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("First commit from local master branch");
 		System.out.println("second commit from local master");
+		System.out.println("third commit from local master");
 
 	}
 
